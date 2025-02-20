f917cfdd93335ef6c94281e4e3fab339de75b592
Nicholas <nickegonzales99@gmail.com>
2025-02-20 13:31:54 -0800

    type added

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 const readline = require("readline");
 const { exit } = require('process');
 
+const builtin = ['echo', 'exit', 'type'];
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -14,6 +15,9 @@ function prompt() {
       exit(0);
     } else if (args[0] === 'echo') {
       console.log(...args.splice(1, args.length));
+    } else if (args[0] === 'type') {
+      if (args.length > 2 || !builtin.includes(args[1])) console.log(`${args[1]}: not found`);
+      else console.log(`${args[1]} is a shell builtin`);
     } else {
       console.log(`${answer}: command not found`);
     }
