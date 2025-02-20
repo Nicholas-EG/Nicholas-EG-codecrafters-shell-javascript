0b648f989a8c7444b20e34a18030ce6a5bdf2979
Nicholas <nickegonzales99@gmail.com>
2025-02-20 15:25:10 -0800

    type extended

--- app/main.js
+++ app/main.js
@@ -1,7 +1,10 @@
 const readline = require("readline");
 const { exit } = require('process');
+const fs = require('fs');
+const path = require('path');
 
 const builtin = ['echo', 'exit', 'type'];
+const pathEnv = process.env.PATH;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,13 +14,26 @@ const rl = readline.createInterface({
 function prompt() {
   rl.question("$ ", (answer) => {
     const args = answer.split(" ");
-    if (answer === 'exit 0') {
-      exit(0);
+    if (args[0] === 'exit') {
+      exit(args[1]);
     } else if (args[0] === 'echo') {
       console.log(...args.splice(1, args.length));
     } else if (args[0] === 'type') {
-      if (args.length > 2 || !builtin.includes(args[1])) console.log(`${args[1]}: not found`);
-      else console.log(`${args[1]} is a shell builtin`);
+      if (builtin.includes(args[1])) console.log(`${args[1]} is a shell builtin`);
+      else {
+        const dirs = pathEnv.split(path.delimiter);
+        let found = false;
+        for (const dir of dirs) {
+          const filePath = path.join(dir, args[1]);
+          try {
+            fs.readFileSync(filePath);
+            console.log(`${args[1]} is ${filePath}`);
+            found = true;
+            break;
+          } catch(err) {}
+        }
+        if (!found) console.log(`${args[1]}: not found`);
+      }
     } else {
       console.log(`${answer}: command not found`);
     }
