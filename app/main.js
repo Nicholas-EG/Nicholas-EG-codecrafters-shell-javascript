5658bac1418db954e75520fc73190933c43f5f8c
Nicholas <nickegonzales99@gmail.com>
2025-02-21 11:33:25 -0800

    refactor to add switch statements

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const { execFileSync, spawnSync } = require('child_process');
 
-const builtin = ['echo', 'exit', 'type'];
+const builtin = ['echo', 'exit', 'type', 'pwd'];
 const pathEnv = process.env.PATH;
 
 const rl = readline.createInterface({
@@ -15,34 +15,40 @@ const rl = readline.createInterface({
 function prompt() {
   rl.question("$ ", (answer) => {
     const args = answer.split(" ");
-    if (answer === 'exit 0') {
-      exit(0);
-    } else if (args[0] === 'echo') {
-      console.log(...args.splice(1, args.length));
-    } else if (args[0] === 'type') {
-      if (builtin.includes(args[1])) {
-        console.log(`${args[1]} is a shell builtin`);
-      }
-      else {
-        const fileName = getFilePath(args[1]);
-        if (fileName === null) {
-          console.log(`${args[1]}: not found`);
+    switch (args[0]) {
+      case 'exit':
+        exit(0);
+      case 'echo':
+        console.log(...args.splice(1, args.length));
+        break;
+      case 'type':
+        if (builtin.includes(args[1])) {
+          console.log(`${args[1]} is a shell builtin`);
         }
         else {
-          console.log(`${args[1]} is ${fileName}`);
+          const fileName = getFilePath(args[1]);
+          if (fileName === null) {
+            console.log(`${args[1]}: not found`);
+          }
+          else {
+            console.log(`${args[1]} is ${fileName}`);
+          }
         }
-      }
-    } else {
-      const filePath = getFilePath(args[0]);
-      if (filePath === null) console.log(`${args[0]}: command not found`);
-      else {
-        try {
-          spawnSync(`${args[0]}`, args.slice(1), { shell: true, stdio: 'inherit' });
-        } catch (err) {
-          console.error(`Error executing command: ${err.message}`);
-          console.error(err.stack);
+        break;
+      case 'pwd':
+        break;
+      default:
+        const filePath = getFilePath(args[0]);
+        if (filePath === null) console.log(`${args[0]}: command not found`);
+        else {
+          try {
+            spawnSync(`${args[0]}`, args.slice(1), { shell: true, stdio: 'inherit' });
+          } catch (err) {
+            console.error(`Error executing command: ${err.message}`);
+            console.error(err.stack);
+          }
         }
-      }
+        break;
     }
     prompt();
   });
